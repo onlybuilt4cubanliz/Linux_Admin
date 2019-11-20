e2be2273e27d2ad1e6ba77e8a4fe8c4f2ff00ac8
onlybuilt4cubanliz <43526567+onlybuilt4cubanliz@users.noreply.github.com>
2019-11-19 23:46:18 -0500

    Create health.sh

---
+++ health.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Network Health Information"
+
+echo "Memory Information"
+  cat /proc/meminfo
+echo " Running Processes"
+  ps
+echo "Network Status"
+  uptime
+  Topdump
+
+echo "Subsystem Information"
+  iostat
+
+echo "Network Health Report"
